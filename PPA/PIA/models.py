c1a6a717af3d2e1b853b24788dccc2dd25f23ca6
MPicasso15 <maanuel.picasso@gmail.com>
2017-02-01 20:31:20 -0600

    ALL MODELS HAVE BEEN ADDED IN ORDER

--- PPA/PIA/models.py
+++ PPA/PIA/models.py
@@ -2,6 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+class User(models.Model):
+	username = models.CharField(max_length=50)
+	password = models.CharField(max_length=50)
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class Project(models.Model):
+	name = models.CharField(max_length=150)
+	number = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
+
 class Indicator(models.Model):
 	description = models.TextField()
 	expected = models.IntegerField()
@@ -23,23 +40,6 @@ class Action(models.Model):
 
 	def __str__(self):
 		return "Descripcion: " + self.description
-
-
-class User(models.Model):
-	username = models.CharField(max_length=50)
-	password = models.CharField(max_length=50)
-	name = models.CharField(max_length=100)
-
-	def __str__(self):
-		return self.name
-
-
-class Project(models.Model):
-	name = models.CharField(max_length=150)
-	number = models.IntegerField()
-
-	def __str__(self):
-		return self.name
 
 
 class Review(models.Model):
