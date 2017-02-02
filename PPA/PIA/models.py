3a2a44cca479b063328339bf1a70434c74443dc2
MPicasso15 <maanuel.picasso@gmail.com>
2017-02-01 20:26:37 -0600

    Indicator and Action have been added

--- PPA/PIA/models.py
+++ PPA/PIA/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Indicator(models.Model):
+	description = models.TextField()
+	expected = models.IntegerField()
+	initial_date = models.DateField()
+	end_date = models.DateField()
+	periodicity = models.IntegerField()
+	project = models.ForeignKey(Project, on_delete = models.CASCADE)
+	responsible = models.ForeignKey(User, on_delete = models.CASCADE)
+
+	def __str__(self):
+		return "Proyecto: " + self.project + " ,Responsable: " 
+		+ self.responsible + " ,Descripción: " + self.description
+
+class Action(models.Model):
+	description = models.TextField()
+	number = models.IntegerField()
+	project = models.ForeignKey(Project, on_delete = models.CASCADE)
+
+	def __str__(self):
+		return "Descripcion: " + self.description
